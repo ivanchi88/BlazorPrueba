49d987b1c6cfd465ec5116c672ef5b244d123822
ivanchi88 <ivanmotos_8@hotmail.es>
2019-05-11 13:27:37 +0200

    Added moving to done feature

--- WebApplication1/Components/Tasks/TaskListBase.cs
+++ WebApplication1/Components/Tasks/TaskListBase.cs
@@ -31,6 +31,10 @@ namespace WebApplication1.Components.Tasks {
             }
         } 
 
+        protected void ChangeTaskStatus(TaskDto task, TaskStatusEnum status) {
+            task.status = status;
+        }
+
         protected void RemoveItem(TaskDto task) {
             tasks.Remove(task);
         }
