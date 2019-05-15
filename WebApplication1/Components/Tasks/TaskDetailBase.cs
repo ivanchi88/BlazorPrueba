91bb92d3f3e4eb6bd34cd9ea15a8ad1d0eb9c747
ivanchi88 <ivanmotos_8@hotmail.es>
2019-05-15 18:40:40 +0200

    Saving and retrieving task from db working as expected

--- WebApplication1/Components/Tasks/TaskDetailBase.cs
+++ WebApplication1/Components/Tasks/TaskDetailBase.cs
@@ -3,11 +3,21 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 using WebApplication1.Data.Tasks;
+using WebApplication1.ServerCode.DataAccess;
 
 
 namespace WebApplication1.Components.Tasks {
 
     public class TaskDetailBase : ComponentBase {
+        [ParameterAttribute]
+        public string id { get; set; }
 
+        protected TaskDto task {get; set;}
+            [Inject]
+            protected IGenericDataAccessor dataAccessor {get; set;}
+        protected override async Task OnInitAsync()
+        {
+            task = dataAccessor.find<TaskDto>(Guid.Parse(id));
+        }
     } 
 }
--- WebApplication1/ServerCode/DataAccess/MySqlDataAccessor.cs
+++ WebApplication1/ServerCode/DataAccess/MySqlDataAccessor.cs
@@ -11,6 +11,8 @@ namespace WebApplication1.ServerCode.DataAccess {
         private MySqlConnection databaseConnection;
         private string connectionString = "datasource=127.0.0.1;port=3306;username=test2;password=test;database=BlazorDB;";
 
+        private string defaultTable = "blazordb.defaulttable";
+
         public void connect() { 
             
         }   
@@ -20,10 +22,12 @@ namespace WebApplication1.ServerCode.DataAccess {
             databaseConnection = new MySqlConnection(connectionString);
 
             data.Type = typeof(T);
+
             var serialized = JsonConvert.SerializeObject(data);
             var globalType = JsonConvert.SerializeObject(data.GetType()); 
+            var serializedUid = JsonConvert.SerializeObject(data.Uid);
 
-            var query = $"INSERT INTO blazordb.DefaultTable (Uid, Type, Data) VALUES ( {JsonConvert.SerializeObject(data.Uid)}, {globalType}, '{serialized}');"; 
+            var query = $"INSERT INTO {defaultTable} (Uid, Type, Data) VALUES ( '{serializedUid}', '{globalType}', '{serialized}');"; 
 
             MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
             commandDatabase.CommandTimeout = 60;
@@ -32,7 +36,6 @@ namespace WebApplication1.ServerCode.DataAccess {
             {
                 databaseConnection.Open();
                 MySqlDataReader myReader = commandDatabase.ExecuteReader();
-                 
         
                 databaseConnection.Close();
             }
@@ -48,6 +51,68 @@ namespace WebApplication1.ServerCode.DataAccess {
         }
 
         public T find <T> (Guid id){
+            var serializedUid = JsonConvert.SerializeObject(id);
+
+            var query = $"SELECT * FROM {defaultTable} where Uid = '{serializedUid}'";
+            
+            databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            // A consultar !
+            try
+            {
+                // Abre la base de datos
+                databaseConnection.Open();
+
+                // Ejecuta la consultas
+                reader = commandDatabase.ExecuteReader();
+
+                // Hasta el momento todo bien, es decir datos obtenidos
+
+                // IMPORTANTE :#
+                // Si tu consulta retorna un resultado, usa el siguiente proceso para obtener datos
+                
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // En nuestra base de datos, el array contiene:  ID 0, Type 1 , Data 3
+                        // Hacer algo con cada fila obtenida
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2)};
+
+                        var uid = JsonConvert.DeserializeObject<Guid>(row[0]);
+                        Type type = JsonConvert.DeserializeObject<Type>(row[1]);
+
+                        var data = JsonConvert.DeserializeObject<SavedDataDto<T>>(row[2]);
+
+                        if (typeof(T) !=  data.Type) {
+                            throw new Exception($"Invalid type {typeof(T)} is not equal to {data.Type}"  );
+                        }
+                        
+                        Console.WriteLine(data);
+                        Console.WriteLine(data.Data);
+
+                        var result = data.Data;
+
+                        return result;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No se encontraron datos.");
+                }
+
+                // Cerrar la conexión
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar cualquier excepción
+                Console.WriteLine(ex.Message);
+            }
+
             return default(T);
         }
 
